b553106b2f839833a4618f66fb19b40ad69f5d54
19524461 <117030431+19524461@users.noreply.github.com>
2022-10-31 00:09:25 +0700

    Create jsVNoff.sh

---
+++ jsVNoff.sh
@@ -0,0 +1,16 @@
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/1e02aeabcf2b885a42ffeab110dd1886/firewall/rules/a7f1bce6765d4a1b928e59f822a70595" \
+     -H "X-Auth-Email: f1kizzvn001@gmail.com" \
+     -H "X-Auth-Key: d6bb2120a8a2e0ed2f96a018274266d2bbc07" \
+     -H "Content-Type: application/json" \
+     --data '{
+     "action": "challenge",
+     "priority": 3000,
+     "paused":true,
+     "description": "captchaVN",
+     "filter": {
+    "id": "984e064edfe1409690bb386bd6d64348",
+    "expression": "(ip.geoip.country eq \"VN\")",
+    "paused": false,
+    "description": "captchaVN"
+  }
+  }'
