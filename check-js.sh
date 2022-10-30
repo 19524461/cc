c497a86ead82d81bfff395960cba8caf00a08851
19524461 <117030431+19524461@users.noreply.github.com>
2022-10-31 00:30:37 +0700

    Update check-js.sh

--- check-js.sh
+++ check-js.sh
@@ -2,7 +2,7 @@
 connect=$(netstat -alntp | grep :443 | wc -l)
 if [ $connect -lt 700 ]
 then
-  cat /root/status-block.txt | grep "on" && bash <(curl -Ls https://raw.githubusercontent.com/19524461/cc/main/jsVNoff.sh) && echo "off" > /root/status-block.txt && clear && echo -e "ANTI DDOS OFF"
+  cat /root/status-js.txt | grep "on" && bash <(curl -Ls https://raw.githubusercontent.com/19524461/cc/main/jsVNoff.sh) && echo "off" > /root/status-js.txt && clear && echo -e "ANTI DDOS OFF"
 else
-  cat /root/status-block.txt | grep "off" && bash <(curl -Ls https://raw.githubusercontent.com/19524461/cc/main/jsVNon.sh) && echo "on" > /root/status-block.txt && clear && echo -e "ANTI DDOS ON"
+  cat /root/status-js.txt | grep "off" && bash <(curl -Ls https://raw.githubusercontent.com/19524461/cc/main/jsVNon.sh) && echo "on" > /root/status-js.txt && clear && echo -e "ANTI DDOS ON"
 fi
