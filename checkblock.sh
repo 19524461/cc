a81f64c7d0f99ab8edfd6623f702f944ba72820a
19524461 <117030431+19524461@users.noreply.github.com>
2022-10-31 00:27:52 +0700

    Update checkblock.sh

--- checkblock.sh
+++ checkblock.sh
@@ -5,3 +5,4 @@ then
   cat /root/status-block.txt | grep "on" && bash <(curl -Ls https://raw.githubusercontent.com/19524461/cc/main/blockoff.sh) && echo "off" > /root/status-block.txt && clear && echo -e "ANTI DDOS OFF"
 else
   cat /root/status-block.txt | grep "off" && bash <(curl -Ls https://raw.githubusercontent.com/19524461/cc/main/blockon.sh) && echo "on" > /root/status-block.txt && clear && echo -e "ANTI DDOS ON"
+fi
