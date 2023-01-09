225ce630f0bf8b9febae1154f32f92b6fdb9b91d
19524461 <117030431+19524461@users.noreply.github.com>
2023-01-09 21:21:38 +0700

    Update blockoff.sh

--- blockoff.sh
+++ blockoff.sh
@@ -1,4 +1,4 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/1e02aeabcf2b885a42ffeab110dd1886/firewall/rules/f57d89ce2b22450f915e87c4df2c9ab0" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/1e02aeabcf2b885a42ffeab110dd1886/firewall/rules/b98dfe98c9aa401eb2b2cb3e592c388c" \
      -H "X-Auth-Email: f1kizzvn001@gmail.com" \
      -H "X-Auth-Key: d6bb2120a8a2e0ed2f96a018274266d2bbc07" \
      -H "Content-Type: application/json" \
@@ -6,9 +6,9 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/1e02aeabcf2b885a42ffeab1
      "action": "block",
      "priority": 2000,
      "paused":true,
-     "description": "BLOCK QT",
+     "description": "BL QT",
      "filter": {
-    "id": "ed6539a0f05c42568848bbee91ec8ee8",
+    "id": "f018b0898f604ece888d23302ffc3fbf",
     "expression": "(not ip.geoip.country in {\"VN\"})",
     "paused": false,
     "description": "BLOCK QT"
